e486f4988fd4865109df54d14077dbe3ace41f2c
ojgenn <ojgenn@gmail.comgit>
2018-07-15 22:12:56 +0300

    тут 2 ошибки. selectableKey и рендерящийся <div> после tbody

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,6 @@
 import React, {Component} from 'react';
 import ContentEditable from 'react-contenteditable';
-import { SelectableGroup, createSelectable } from 'react-selectable';
+import {SelectableGroup, createSelectable} from 'react-selectable';
 import '../App.css';
 
 const SelectableComponent = createSelectable(ContentEditable);
@@ -9,13 +9,14 @@ export default class Table extends Component {
   state = {
     tableArray: [],
     row: null,
-    col: null
+    col: null,
+    selectedKeys: []
   };
 
   handleChange = e => {
     console.log(e.target.value);
     let table = [...this.state.tableArray];
-    table[this.state.row][this.state.col]= e.target.value;
+    table[this.state.row][this.state.col] = e.target.value;
     this.setState({tableArray: table})
   };
 
@@ -23,6 +24,10 @@ export default class Table extends Component {
     this.setState({row, col}, function () {
       if (row !== null && col !== null) console.log(this.state.tableArray[row][col])
     })
+  };
+
+  handleSelection (selectedKeys) {
+    this.setState({ selectedKeys });
   };
 
   componentDidMount() {
@@ -40,20 +45,28 @@ export default class Table extends Component {
 
   render() {
     return (
-      <tbody>
-      {this.state.tableArray.map((row, rowIndex) => (
-        <tr key = {rowIndex}>
-          {row.map((item, index) => (
-          <ContentEditable key = {index} html={item} // innerHTML of the editable div
-                           disabled={false}       // use true to disable edition
-                           onChange={this.handleChange}
-                           tagName = 'td'
-                           onFocus = {() => {this.setRowColIndex(rowIndex, index)}}
-                           onBlur =  {() => {this.setRowColIndex(null, null)}} />
+      <SelectableGroup onSelection={this.handleSelection.bind(this)} component = {'tbody'}>
+        {this.state.tableArray.map((row, rowIndex) => (
+          <tr key={rowIndex}>
+            {row.map((item, index) => {
+              let selected = this.state.selectedKeys.indexOf(rowIndex + '_' + index) > -1;
+              return (
+              <SelectableComponent key={index} html={item} // innerHTML of the editable div
+                               disabled={false}       // use true to disable edition
+                               selected={selected}
+                               selectableKey={rowIndex + '_' + index}
+                               onChange={this.handleChange}
+                               tagName='td'
+                               onFocus={() => {
+                                 this.setRowColIndex(rowIndex, index)
+                               }}
+                               onBlur={() => {
+                                 this.setRowColIndex(null, null)
+                               }}/>
+            )})}
+          </tr>
         ))}
-        </tr>
-        ))}
-      </tbody>
+      </SelectableGroup>
     )
   }
 };
